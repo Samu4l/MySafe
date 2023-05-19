1c4caaa45ca58a2d12bd5fb2d2454a09870f6461
Samu4l <samuel.ballaman01@studentfr.ch>
2023-05-19 21:51:56 +0200

    modif

--- js/login.js
+++ js/login.js
@@ -4,53 +4,43 @@ $(document).ready(function() {
     pin.on("input", function() {
         var pinValue = pin.val();
         console.log(pinValue); // Correction : Utilisation de pinValue au lieu de pin.val dans console.log
-        if (pinValue.length === 4) {
+        if (pinValue.length === 6) {
             getJsonData(connectSuccess, callbackError); // Ajout de pinValue comme argument pour getJsonData
         }
+
     });
 
-    btnFinger.on("click",function(){
-        if (window.PublicKeyCredential) {
-            authenticateWithFingerprint(login);
-        } else {
-            console.log("L'authentification par empreinte digitale n'est pas prise en charge par votre navigateur.");
+ var btnFinger = $("#fingerprintButton");
+
+  btnFinger.on("click", function() {
+    if (window.PublicKeyCredential) {
+      var options = {
+        publicKey: {
+          challenge: new Uint8Array(32), // Générer un challenge sécurisé
+          allowCredentials: [
+            // Liste des empreintes enregistrées par l'utilisateur
+            { type: "public-key", id: Uint8Array.from(/* ID de l'empreinte */) }
+          ]
         }
-    });
+      }
+    
 
-    function authenticateWithFingerprint(callback) {
-        var publicKey = {
-            challenge: new Uint8Array(32), // Générer un challenge sécurisé ici
-            rp: {
-                name: "MySafe"
-            },
-            user: {
-                id: new Uint8Array(16), // Identifiant utilisateur unique ici
-                name: "utilisateur"
-            },
-            authenticatorSelection: {
-                authenticatorAttachment: "platform",
-                userVerification: "required"
-            },
-            pubKeyCredParams: [
-                {
-                    type: "public-key",
-                    alg: -7 // ECDSA with SHA-256
-                }
-            ]
-        };
 
-        navigator.credentials.get({ publicKey: publicKey })
-            .then(function(credential) {
-                console.log("Authentification réussie !");
-                callback(true);
-            })
-            .catch(function(error) {
-                console.log("Erreur d'authentification par empreinte digitale :", error);
-                callback(false);
-            });
+      navigator.credentials.get({ publicKey: options }).then(function(assertion) {
+        // L'authentification biométrique est réussie
+        // Faire quelque chose avec l'assertion retournée
+      }).catch(function(error) {
+        // L'authentification biométrique a échoué ou a été annulée par l'utilisateur
+      });
+    } else {
+      // L'API n'est pas supportée par le navigateur
+      alert("L'authentification par empreinte digitale n'est pas supportée par votre navigateur.");
     }
+});
 
-    function login(ok) {
+   
+
+    function loginReussi(ok) {
         if (ok) {
             console.log("Connexion réussie !");
             login(ok);
