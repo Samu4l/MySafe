19c5253d2bb0f07800fc14dd3b3b4b1f27bf5408
Samu4l <samuel.ballaman01@studentfr.ch>
2023-05-20 11:26:49 +0200

    mo

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ function login(ok){
         log("ça avance bb");
     }
 }
+$(document).ready(function() {
+const registerButton = $("#registerButton");
+
+// Ajoutez un gestionnaire d'événements au clic sur le bouton
+registerButton.on("click", function() {
+  // Redirigez l'utilisateur vers la page "register.html"
+  window.location.href = "register.html";
+
+});
+});
